484b923b1b1cf3b400d17f59722315a0fae41283
Tymek <tymstr48@gmail.com>
2020-11-10 20:10:58 +0100

    big changes

--- js/calc.js
+++ js/calc.js
@@ -1,22 +1,3 @@
-var digital = document.querySelector('.result');
-var num = document.querySelectorAll('button');
-
-var btn_names = [];
-
-for (i = 0; i < num.length; i++) {
-    btn_names[i] = num[i].name;
-}
-
-
-for (i = 0; i < num.length; i++){
-    var r = num[i].name; 
-    num[i].addEventListener("click", event =>{       
-        calc( event.target.name );
-    });
-}
-
-function calc(nr){
-
-        console.log(nr);
-    
-}
+var digital = document.querySelector('input.result');
+var num = document.querySelectorAll('button.num');
+var toggler = false;
